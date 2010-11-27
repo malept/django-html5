cb69a7482ce0e60b18c6ced23df8f8552689d2a1
Mark Lee <django-html5@lazymalevolence.com>
2010-11-26 18:39:47 -0800

    forms.fields: monkeypatch instead of subclass; add DecimalField support

--- html5/forms/fields.py
+++ html5/forms/fields.py
@@ -1,19 +1,18 @@
-from django.forms.fields import IntegerField as DjangoIntegerField
+from django.forms.fields import DecimalField, IntegerField
 
-from html5.forms import NumberInput
 
-class IntegerField(DjangoIntegerField):
-    widget = NumberInput
-
-    def widget_attrs(self, widget):
+def _set_minmax_widget_attrs(cls):
+    def _minmax_widget_attrs(self, widget):
         """
         Given a Widget instance (*not* a Widget class), returns a dictionary of
         any HTML attributes that should be added to the Widget, based on this
         Field.
         """
-        attrs = {}
+        attrs = super(cls, self).widget_attrs(widget)
         if self.min_value is not None:
             attrs['min'] = self.min_value
         if self.max_value is not None:
             attrs['max'] = self.max_value
         return attrs
+    cls.widget_attrs = _minmax_widget_attrs
+[_set_minmax_widget_attrs(cls) for cls in [DecimalField, IntegerField]]
